f4837399edaf149e2fcc7e573d81d69a4eb338e0
Liz Kaufman <lizkaufman92@gmail.com>
2023-03-12 13:03:03 +0000

    skeleton of header component

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-center ">
+      <h2 className="px-3 py-4">Games Workshop</h2>
+    </div>
+  );
+};
+
+export default Header;
