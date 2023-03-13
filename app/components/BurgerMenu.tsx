0108c26fa3cf8eea8ba471e4965be3481e270275
Liz Kaufman <lizkaufman92@gmail.com>
2023-03-13 21:27:02 +0000

    basic burger menu with x animation in header

---
+++ app/components/BurgerMenu.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import styles from "../styles/burgerMenu.module.css";
+
+function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleMenuClick() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div
+      className={`${styles.burgerMenu} ${isOpen ? styles.open : ""}`}
+      onClick={handleMenuClick}
+    >
+      <span className={styles.bar}></span>
+      <span className={styles.bar}></span>
+      <span className={styles.bar}></span>
+    </div>
+  );
+}
+
+export default BurgerMenu;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import styles from "../styles/header.module.css";
+
+import BurgerMenu from "./BurgerMenu";
 
 const Header = () => {
   return (
     <div className="flex flex-row justify-center ">
       <h2 className="px-3 py-4">Games Workshop</h2>
+      <BurgerMenu />
     </div>
   );
 };
